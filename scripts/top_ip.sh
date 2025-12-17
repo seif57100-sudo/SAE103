7e8c42dacaf4b76d944e3b5ef172760a6379f344
Ahmet BASBUNAR <ahmetbsbnr@icloud.com>
2025-12-17 15:04:21 +0100

    Enhance top_ip.sh with user input for IP count
    Add user prompt for number of top IP addresses to display.

--- scripts/top_ip.sh
+++ scripts/top_ip.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
 
+#Creation d'une variable a lu par l'utilisateur
+
+read -p "Combien des adresses IP les plus utilisÃ©s souhaitez-vous afficher : " a
+
+#affichages des a adresses ip les plus utilisÃ©
+
+grep "FAIL" auth.log | cut -d';' -f5 | sort | uniq -c | sort -r | head -n $a
