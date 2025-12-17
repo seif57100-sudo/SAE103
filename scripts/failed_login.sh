b2d3a6f72be3613908939a6993579304a3de06e2
Ahmet BASBUNAR <ahmetbsbnr@icloud.com>
2025-12-17 15:06:00 +0100

    Add failed_login.sh to analyze auth.log
    Script to analyze failed login attempts and display results.

--- scripts/failed_login.sh
+++ scripts/failed_login.sh
@@ -1,1 +1,20 @@
+#!/usr/bin/env bash
 
+echo -e "\n\n\n\n\nVerification des connexions..."
+
+#Declaration des variables pour les afficher dans echo
+
+a=$(grep "FAIL" auth.log | wc -l)
+b=$(grep "FAIL" auth.log | cut -d';' -f4 | sort -u | wc -l)
+
+echo -e "\nIl y a eu $a erreur de connexion de $b personnes distinctes"
+echo "Voici les personnes qui ont eu des erreurs de connexions :"
+
+#Commande pour montrer les personnes distinctement avec leur nombre d'Ã©chec de connexion
+
+grep "FAIL" auth.log | cut -d';' -f4 | sort | uniq -c
+
+#Selection de toutes les lignes oÃ¹ il y a eu des erreurs
+
+echo -e "\nVoici toutes les lignes oÃ¹ se trouves les erreurs"
+grep "FAIL" auth.log
